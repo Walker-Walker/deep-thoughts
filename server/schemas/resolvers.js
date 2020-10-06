41b8ae0c10458261aceb6f97e8c56663d6b66792
Walker-Walker <awalker0@gmail.com>
2020-10-06 16:22:17 -0700

    added mutations to typeDefs schema and resolvers but getting error Query.Mutation defined in resolvers, but not in schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,6 +27,19 @@ const resolvers = {
               .populate('thoughts');
           },
 
+          Mutation: {
+              addUser: async (parent, args) => {
+                  const user = await User.create(args);
+                  return user;
+
+              },
+
+              login: async () => {
+
+              }
+          }
+
+
         }
     };
 
