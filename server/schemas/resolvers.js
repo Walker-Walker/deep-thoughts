b162eb08e207ee82dc0429f6d2ae274e83debd9d
Walker-Walker <awalker0@gmail.com>
2020-10-06 22:55:35 -0700

    got stuck and cannot create a new user to access pw credentials..inorder to continue

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models')
 const { AuthenticationError } = require('apollo-server-express')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
 
@@ -27,12 +28,27 @@ const resolvers = {
               .populate('thoughts');
           },
 
+          me: async (parent, args, context) => {
+              if (context.user) {
+
+                  const userData = await User.findOne({_id: context.user_id})
+                  .select('-__v -password')
+                  .populate('thoughts')
+                  .populate('friends');
+                  return userData;
+              }
+throw new AuthenticationError('Not logged in')
+
+          },
+
         },
           
           Mutation: {
               addUser: async (parent, args) => {
                   const user = await User.create(args);
-                  return user;
+                  const token = signToken(user);
+
+                  return { token, user };
 
               },
 
@@ -48,9 +64,52 @@ const resolvers = {
                   if (!correctPw) {
                       throw new AuthenticationError('Incorrect credentials')
                   }
-                  return user;
+                  const token = signToken(user);
+
+                  return { token, user };
 
               },
+
+              addThought: async (parent, args, context) => {
+                  if (context.user) {
+                      const thought = await Thought.create({ ...args, username: context.user.username});
+
+                      await User.findByIdAndUpdate(
+                          {_id: context.user._id},
+                          {$push:{thoughts:thought._id}},
+                          {new: true}
+                      );
+                      return thought;
+                  }
+                  throw new AuthenticationError('You need to be logged in!');
+              },
+
+              addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+                if (context.user) {
+                  const updatedThought = await Thought.findOneAndUpdate(
+                    { _id: thoughtId },
+                    { $push: { reactions: { reactionBody, username: context.user.username } } },
+                    { new: true, runValidators: true }
+                  );
+              
+                  return updatedThought;
+                }
+              
+                throw new AuthenticationError('You need to be logged in!');
+              },
+              addFriend: async (parent, { friendId }, context) => {
+                if (context.user) {
+                  const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { friends: friendId } },
+                    { new: true }
+                  ).populate('friends');
+              
+                  return updatedUser;
+                }
+              
+                throw new AuthenticationError('You need to be logged in!');
+              }
           }
           
     };
